808ccb6867a85a4304aa05266d20f0a772f354b8
Nicholas Hartman <nickilous@gmail.com>
2012-11-14 22:26:07 -0800

    add message handling and wifi server thread

--- src/com/nickilous/alertification/AlertificationService.java
+++ src/com/nickilous/alertification/AlertificationService.java
@@ -1,14 +1,62 @@
 package com.nickilous.alertification;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
-import android.widget.Toast;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class AlertificationService extends Service {
 
-    private SharedPreferences sharedPref;
+    private SharedPreferences sharedPref = PreferenceManager
+            .getDefaultSharedPreferences(this);;
+    // default ip
+    public static String SERVERIP = "10.0.2.15";
+
+    // designate a port
+    public static final int SERVERPORT = 8080;
+
+    final Messenger mMessenger = new Messenger(new IncomingHandler()); // Target
+                                                                       // we
+                                                                       // publish
+                                                                       // for
+                                                                       // clients
+                                                                       // to
+                                                                       // send
+                                                                       // messages
+                                                                       // to
+                                                                       // IncomingHandler.
+
+    private ServerSocket serverSocket;
+    private static boolean isRunning = false;
+
+    ArrayList<Messenger> mClients = new ArrayList<Messenger>(); // Keeps track
+                                                                // of all
+                                                                // current
+                                                                // registered
+                                                                // clients.
+    int mValue = 0; // Holds last value set by a client.
+    static final int MSG_REGISTER_CLIENT = 1;
+    static final int MSG_UNREGISTER_CLIENT = 2;
+    static final int MSG_SET_INT_VALUE = 3;
+    static final int MSG_SET_STRING_VALUE = 4;
 
     public AlertificationService() {
 
@@ -24,14 +72,27 @@ public class AlertificationService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        Log.i("MyService", "Received start id " + startId + ": " + intent);
+        SERVERIP = getLocalIpAddress();
 
+        Thread fst = new Thread(new WifiServerThread());
+        fst.start();
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
-        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+
+        try {
+            // make sure you close the socket upon exiting
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        super.onDestroy();
+
+        Log.i("MyService", "Service Stopped.");
+        isRunning = false;
     }
 
     public void setIsWifiP2pEnabled(boolean b) {
@@ -40,8 +101,153 @@ public class AlertificationService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mMessenger.getBinder();
+    }
+
+    /**
+     * Handler of incoming messages from clients.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MSG_REGISTER_CLIENT:
+                mClients.add(msg.replyTo);
+                break;
+            case MSG_UNREGISTER_CLIENT:
+                mClients.remove(msg.replyTo);
+                break;
+            case MSG_SET_VALUE:
+                mValue = msg.arg1;
+                for (int i = mClients.size() - 1; i >= 0; i--) {
+                    try {
+                        mClients.get(i).send(
+                                Message.obtain(null, MSG_SET_VALUE, mValue, 0));
+                    } catch (RemoteException e) {
+                        // The client is dead. Remove it from the list;
+                        // we are going through the list from back to front
+                        // so this is safe to do inside the loop.
+                        mClients.remove(i);
+                    }
+                }
+                break;
+            default:
+                super.handleMessage(msg);
+            }
+        }
+    }
+
+    private void sendMessageToUI(int intvaluetosend) {
+        for (int i = mClients.size() - 1; i >= 0; i--) {
+            try {
+                // Send data as an Integer
+                mClients.get(i).send(
+                        Message.obtain(null, MSG_SET_INT_VALUE, intvaluetosend,
+                                0));
+
+                // Send data as a String
+                Bundle b = new Bundle();
+                b.putString("str1", "ab" + intvaluetosend + "cd");
+                Message msg = Message.obtain(null, MSG_SET_STRING_VALUE);
+                msg.setData(b);
+                mClients.get(i).send(msg);
+
+            } catch (RemoteException e) {
+                // The client is dead. Remove it from the list; we are going
+                // through the list from back to front so this is safe to do
+                // inside the loop.
+                mClients.remove(i);
+            }
+        }
+    }
+
+    public static boolean isRunning() {
+        return isRunning;
+    }
+
+    public class WifiServerThread implements Runnable {
+
+        public void run() {
+            try {
+                if (SERVERIP != null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            serverStatus
+                                    .setText("Listening on IP: " + SERVERIP);
+                        }
+                    });
+                    serverSocket = new ServerSocket(SERVERPORT);
+                    while (true) {
+                        // listen for incoming clients
+                        Socket client = serverSocket.accept();
+                        handler.post(new Runnable() {
+                            public void run() {
+                                serverStatus.setText("Connected.");
+                            }
+                        });
+
+                        try {
+                            BufferedReader in = new BufferedReader(
+                                    new InputStreamReader(
+                                            client.getInputStream()));
+                            String line = null;
+                            while ((line = in.readLine()) != null) {
+                                Log.d("ServerActivity", line);
+                                handler.post(new Runnable() {
+                                    public void run() {
+                                        // do whatever you want to the front end
+                                        // this is where you can be creative
+                                    }
+                                });
+                            }
+                            break;
+                        } catch (Exception e) {
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    serverStatus
+                                            .setText("Oops. Connection interrupted. Please reconnect your phones.");
+                                }
+                            });
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            serverStatus
+                                    .setText("Couldn't detect internet connection.");
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                handler.post(new Runnable() {
+                    public void run() {
+                        serverStatus.setText("Error");
+                    }
+                });
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // gets the ip address of your phone's network
+    private String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("ServerActivity", ex.toString());
+        }
+        return null;
     }
 
 }
--- src/com/nickilous/alertification/MainActivity.java
+++ src/com/nickilous/alertification/MainActivity.java
@@ -1,32 +1,55 @@
 package com.nickilous.alertification;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    Messenger mService = null;
+    boolean mIsBound;
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CheckIfServiceIsRunning();
 
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
+        CheckIfServiceIsRunning();
     }
 
     @Override
     public void onPause() {
         super.onPause();
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            doUnbindService();
+        } catch (Throwable t) {
+            Log.e("MainActivity", "Failed to unbind from the service", t);
+        }
     }
 
     @Override
@@ -64,4 +87,109 @@ public class MainActivity extends Activity {
         startService(startServerIntent);
     }
 
+    private void CheckIfServiceIsRunning() {
+        // If the service is running when the activity starts, we want to
+        // automatically bind to it.
+        if (AlertificationService.isRunning()) {
+            doBindService();
+        }
+    }
+
+    void doBindService() {
+        // Establish a connection with the service. We use an explicit
+        // class name because there is no reason to be able to let other
+        // applications replace our component.
+        bindService(new Intent(this, MessengerService.class), mConnection,
+                Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+        mCallbackText.setText("Binding.");
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            // If we have received the service, and hence registered with
+            // it, then now is the time to unregister.
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null,
+                            MessengerService.MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                    // There is nothing special we need to do if the service
+                    // has crashed.
+                }
+            }
+
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+            mCallbackText.setText("Unbinding.");
+        }
+    }
+
+    /**
+     * Handler of incoming messages from service.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MessengerService.MSG_SET_VALUE:
+                mCallbackText.setText("Received from service: " + msg.arg1);
+                break;
+            default:
+                super.handleMessage(msg);
+            }
+        }
+    }
+
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service. We are communicating with our
+            // service through an IDL interface, so get a client-side
+            // representation of that from the raw service object.
+            mService = new Messenger(service);
+            mCallbackText.setText("Attached.");
+
+            // We want to monitor the service for as long as we are
+            // connected to it.
+            try {
+                Message msg = Message.obtain(null,
+                        MessengerService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+
+                // Give it some value as an example.
+                msg = Message.obtain(null, MessengerService.MSG_SET_VALUE,
+                        this.hashCode(), 0);
+                mService.send(msg);
+            } catch (RemoteException e) {
+                // In this case the service has crashed before we could even
+                // do anything with it; we can count on soon being
+                // disconnected (and then reconnected if it can be restarted)
+                // so there is no need to do anything here.
+            }
+
+            // As part of the sample, tell the user what happened.
+            Toast.makeText(Binding.this, R.string.remote_service_connected,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+            mCallbackText.setText("Disconnected.");
+
+            // As part of the sample, tell the user what happened.
+            Toast.makeText(Binding.this, R.string.remote_service_disconnected,
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
 }
